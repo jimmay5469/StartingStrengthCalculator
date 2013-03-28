a4c71858871f2b5ba6da9c94b4dcb1df49364576
jimmay5469 <jimmay5469@gmail.com>
2013-03-28 15:19:41 -0400

    configurable multipliers added

--- public/scripts/ssCalc.js
+++ public/scripts/ssCalc.js
@@ -5,7 +5,10 @@ $(function () {
 	var defaultSettings = {
 		startWeight: [45,45,45,45,45],
 		previousWeight: [200,200,200,200,200],
-		weightIncrease: [5,5,5,5,5]
+		weightIncrease: [5,5,5,5,5],
+		onebyfivemultiplier: [25,25,25,25,25],
+		onebythreemultiplier: [50,50,50,50,50],
+		onebytwomultiplier: [75,75,75,75,75]
 	};
 	var settings = defaultSettings;
 	if($.cookie('ssCalcSettings')) {
@@ -27,24 +30,30 @@ $(function () {
 		self.startWeight = ko.observable(settings.startWeight[0]);
 		self.previousWeight = ko.observable(settings.previousWeight[0]);
 		self.weightIncrease = ko.observable(settings.weightIncrease[0]);
+		self.onebyfivemultiplier = ko.observable(settings.onebyfivemultiplier[0]);
+		self.onebythreemultiplier = ko.observable(settings.onebythreemultiplier[0]);
+		self.onebytwomultiplier = ko.observable(settings.onebytwomultiplier[0]);
 		
 		self.workWeight = ko.computed(function () {
 			return parseFloat(self.previousWeight()) + parseFloat(self.weightIncrease());
 		});
 		self.onebyfive = ko.computed(function() {
-			return calculateWarmupWeight(self.startWeight(), self.workWeight(), 0.25);
+			return calculateWarmupWeight(self.startWeight(), self.workWeight(), self.onebyfivemultiplier());
 		});
 		self.onebythree = ko.computed(function() {
-			return calculateWarmupWeight(self.startWeight(), self.workWeight(), 0.5);
+			return calculateWarmupWeight(self.startWeight(), self.workWeight(), self.onebythreemultiplier());
 		});
 		self.onebytwo = ko.computed(function() {
-			return calculateWarmupWeight(self.startWeight(), self.workWeight(), 0.75);
+			return calculateWarmupWeight(self.startWeight(), self.workWeight(), self.onebytwomultiplier());
 		});
 
 		self.selectedLift.subscribe(function(newValue) {
 			self.startWeight(settings.startWeight[newValue.value]);
 			self.previousWeight(settings.previousWeight[newValue.value]);
 			self.weightIncrease(settings.weightIncrease[newValue.value]);
+			self.onebyfivemultiplier(settings.onebyfivemultiplier[newValue.value]);
+			self.onebythreemultiplier(settings.onebythreemultiplier[newValue.value]);
+			self.onebytwomultiplier(settings.onebytwomultiplier[newValue.value]);
 		});
 
 		self.startWeight.subscribe(function() {
@@ -59,6 +68,18 @@ $(function () {
 			settings.weightIncrease[self.selectedLift().value] = self.weightIncrease();
 			$.cookie('ssCalcSettings', settings);
 		});
+		self.onebyfivemultiplier.subscribe(function() {
+			settings.onebyfivemultiplier[self.selectedLift().value] = self.onebyfivemultiplier();
+			$.cookie('ssCalcSettings', settings);
+		});
+		self.onebythreemultiplier.subscribe(function() {
+			settings.onebythreemultiplier[self.selectedLift().value] = self.onebythreemultiplier();
+			$.cookie('ssCalcSettings', settings);
+		});
+		self.onebytwomultiplier.subscribe(function() {
+			settings.onebytwomultiplier[self.selectedLift().value] = self.onebytwomultiplier();
+			$.cookie('ssCalcSettings', settings);
+		});
 	}
 	ko.applyBindings(new ssCalcViewModel());
 
@@ -66,7 +87,7 @@ $(function () {
 		startWeight = parseFloat(startWeight);
 		workWeight = parseFloat(workWeight);
 		multiplier = parseFloat(multiplier);
-		return Math.floor((workWeight - startWeight) * multiplier / 5) * 5 + startWeight;
+		return Math.floor((workWeight - startWeight) * (multiplier / 100) / 5) * 5 + startWeight;
 	}
 });
 // Warmup Set Formula = ((Work Weight - Starting Weight) x Multiplier) + Starting Weight
